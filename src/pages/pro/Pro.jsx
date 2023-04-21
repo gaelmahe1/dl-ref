c384003a26a93f10c6dc62ff968446f04cd042be
Gael Mahe <gaelmahe1@hotmail.com>
2023-04-21 15:57:55 +0200

    modified: home page

--- src/pages/pro/Pro.jsx
+++ src/pages/pro/Pro.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import "./Pro.css";
+import {Footer, Header} from "../../components"
+
 
 const pro = () => {
   return (
     <div>
-      
+      <Header />
+      <Footer />
     </div>
   )
 }
